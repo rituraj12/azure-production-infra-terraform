39399b14365a23eca047769e24ee2718663edf15
rituraj12 <globalgraphicmedia@gmail.com>
2025-11-20 21:02:17 +0530

    Normalize line endings

---
+++ autoscale.tf
@@ -0,0 +1,62 @@
+# create autoscale resource that will decrease the number of instances if the azurerm_orchestrated_scale set cpu usaae is below 10% for 2 minutes
+resource "azurerm_monitor_autoscale_setting" "autoscale" {
+  name                = "autoscale"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  target_resource_id  = azurerm_orchestrated_virtual_machine_scale_set.vmss_terraform_tutorial.id
+  enabled             = true
+  profile {
+    name = "autoscale"
+    capacity {
+      default = 3
+      minimum = 1
+      maximum = 10
+    }
+
+    # -------------------------
+    # SCALE OUT RULE
+    # CPU >= 80% for 2 minutes
+    # -------------------------
+    rule {
+      metric_trigger {
+        metric_name        = "Percentage CPU"
+        metric_resource_id = azurerm_orchestrated_virtual_machine_scale_set.vmss_terraform_tutorial.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT2M"   # 2 minutes
+        time_aggregation   = "Average"
+        operator           = "GreaterThan"
+        threshold          = 80
+      }
+      scale_action {
+        direction = "Increase"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT2M"
+      }
+    }
+
+    # -------------------------
+    # SCALE IN RULE
+    # CPU <= 10% for 2 minutes
+    # -------------------------
+    rule {
+      metric_trigger {
+        metric_name        = "Percentage CPU"
+        metric_resource_id = azurerm_orchestrated_virtual_machine_scale_set.vmss_terraform_tutorial.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT2M"   # 2 minutes
+        time_aggregation   = "Average"
+        operator           = "LessThan"
+        threshold          = 10
+      }
+      scale_action {
+        direction = "Decrease"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT2M"
+      }
+    }
+  }
+}
---
+++ provider.tf
@@ -0,0 +1,18 @@
+# Configure the Azure provider, you can have many
+# if you use azurerm provider, it's source is hashicorp/azurerm
+# short for registry.terraform.io/hashicorp/azurerm
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      # version = "~> 4.8.0"
+      version = "= 3.114.0"
+    }
+  }
+  required_version = ">= 1.9.0"
+}
+
+provider "azurerm" {
+  features {}
+}
